03c7e8c807c0bee762ef73212fd930f9f12b63c6
Jame <Jamesferris447@gmail.com>
2026-09-07 19:53:52 +1000

    formatted code better and added lore

--- main.py
+++ main.py
@@ -6,123 +6,170 @@ import time
 def cls():
     os.system('cls' if os.name =='nt' else 'clear')
 
-goal = random.randint(3,12)
-
-
-print(f"Roll the dice until you get the number {goal} to start the program")
-print(f"Once you get {goal} press the S key to start")
-print('_______________________________________________________')
-roll = input('Press enter to roll')
-
-# roll to start
-while True:
-    dice1 = random.randint(1, 4)
-    dice2 = random.randint(1, 4)
-    dice3 = random.randint(1,4)
-    diceTotal = dice1 + dice2 + dice3
-    print(f"you rolled... {diceTotal}")
-    print('')
-    print('Save roll?') 
-    start = input('[Y/N] ').lower()
+def dice():
+    goal = random.randint(3,12)
+    
+    
+    print(f"Roll the dice until you get the number {goal} to start the program")
+    print(f"Once you get {goal} press the S key to start")
     print('_______________________________________________________')
-    if start == 's':
-        if diceTotal == goal:
-            print('PROGRAM STARTING...')
+    roll = input('Press enter to roll')
+    
+    # roll to start
+    while True:
+        dice1 = random.randint(1, 4)
+        dice2 = random.randint(1, 4)
+        dice3 = random.randint(1,4)
+        diceTotal = dice1 + dice2 + dice3
+        print(f"you rolled... {diceTotal}")
+        print('')
+        print('Save roll?') 
+        start = input('[Y/N] ').lower()
+        print('_______________________________________________________')
+        if start == 's':
+            if diceTotal == goal:
+                print('PROGRAM STARTING...')
+                break
+            else:
+                sys.exit('Wrong number buddy')
+        if start != 'y':
+            roll = input('Press enter to roll')
+        elif start == 'y':
+            print('read the instructions, bucko')
             break
-        else:
-            sys.exit('Wrong number buddy')
-    if start != 'y':
-        roll = input('Press enter to roll')
-    elif start == 'y':
-        sys.exit('Read the instructions next time')
 
-# loading cycle
-loadTime = random.randint(5, 10)
-loadCurrent = 0
+def load():
+    # loading cycle
+    loadTime = random.randint(5, 10)
+    loadCurrent = 0
 
-while loadCurrent != loadTime:
+    while loadCurrent != loadTime:
+        cls()
+        print('Loading')
+        time.sleep(1)
+        cls()
+        print('Loading.')
+        time.sleep(1)
+        cls()
+        print('Loading..')
+        time.sleep(1)
+        cls()
+        print('Loading...')
+        time.sleep(1)
+        cls()
+        print('Loading....\n (Password:1752764)')
+        time.sleep(1)
+        loadCurrent += 1
+
     cls()
-    print('Loading')
-    time.sleep(1)
+    print('Starting program!')
+    time.sleep(3)
+
+def captcha():
+        # CAPTHA
     cls()
-    print('Loading.')
-    time.sleep(1)
-    cls()
-    print('Loading..')
-    time.sleep(1)
-    cls()
-    print('Loading...')
-    time.sleep(1)
-    cls()
-    print('Loading....')
-    time.sleep(1)
-    loadCurrent += 1
+    print('_______________________________________________________')
+    print('ARE YOU HUMAN?')
+    CAPTCHA1 = input('[Y/N] ').lower()
 
-cls()
-print('Starting program!')
-time.sleep(3)
-
-# CAPTHA
-cls()
-print('_______________________________________________________')
-print('ARE YOU HUMAN?')
-CAPTCHA1 = input('[Y/N] ').lower()
-
-if CAPTCHA1 == 'y':
-    print('Why do you follow instructions so well then?')
-    sys.exit('ROBOT DETECTED')
-elif CAPTCHA1 == 'n':
-    sys.exit('ROBOT DETECTED')
-else:
-    cls()
-    print('hmm alright, solve the following equation to prove you are human')
-
-# math
-math1 = random.randint(100, 999)
-math2 = random.randint(1000, 9999)
-mathTotal = math1 * math2
-
-print(f'what is {math1} multiplied by {math2} equal to?')
-
-while True:
-    try:
-        mathAnswer = int(input('INPUT ANSWER: '))
-        break
-    except ValueError:
-        print('That is not a number bro')
-
-
-if mathAnswer == mathTotal:
-    print('No human is that good at math')
-    sys.exit('ROBOT DETECTED')
-
-else:
-    cls()
-    print("That's so extremely wrong... very human of you")
-
-
-rickOptions = ['a', 'b', 'c', 'd', 'e', 'f',]
-rickCorrect = random.choice(rickOptions)
-print('Finish the lyrics if you really are that human')
-print('')
-print('"Never gonna..."')
-print('A. Give you up')
-print('B. Let you down')
-print('C. Run around and desert you')
-print('D. Make you cry')
-print('E. Say goodbye')
-print('F. Tell a lie and hurt you')
-
-rickGuess = input('A, B, C, D, E or F? ').lower()
-lives = 3
-
-while rickGuess != rickCorrect:
-    if rickGuess in ['a', 'b', 'c', 'd', 'e', 'f']:
-        lives -= 1
-        print(f'INCORRECT {lives} ATTEMPTS REMAINING')
-
+    if CAPTCHA1 == 'y':
+        print('Why do you follow instructions so well then?')
+        sys.exit('ROBOT DETECTED')
+    elif CAPTCHA1 == 'n':
+        sys.exit('ROBOT DETECTED')
     else:
-        print('It is not this difficult bro...')
+        cls()
+
+def math():
+     print('hmm alright, solve the following equation to prove you are human')
+    
+        # math
+    math1 = random.randint(100, 999)
+    math2 = random.randint(1000, 9999)
+    mathTotal = math1 * math2
+    
+    print(f'what is {math1} multiplied by {math2} equal to?')
+    
+    while True:
+        try:
+            mathAnswer = int(input('INPUT ANSWER: '))
+            break
+        except ValueError:
+            print('That is not a number bro')
+    
+    
+    if mathAnswer == mathTotal:
+        print('No human is that good at math')
+        sys.exit('ROBOT DETECTED')
+    
+    else:
+        cls()
+        print("That's so extremely wrong... very human of you")
+        
+
+finished = False
+
+while not finished:
+
+    dice()
+    load()
+    captcha()
+
+
+
+    rickOptions = ['a', 'b', 'c', 'd', 'e', 'f',]
+    rickCorrect = random.choice(rickOptions)
+    print('Finish the lyrics if you really are that human')
+    print('')
+    print('"Never gonna..."')
+    print('A. Give you up')
+    print('B. Let you down')
+    print('C. Run around and desert you')
+    print('D. Make you cry')
+    print('E. Say goodbye')
+    print('F. Tell a lie and hurt you')
 
     rickGuess = input('A, B, C, D, E or F? ').lower()
+    lives = 3
 
+    while rickGuess != rickCorrect:
+        if rickGuess in ['a', 'b', 'c', 'd', 'e', 'f']:
+            lives -= 1
+            print(f'INCORRECT {lives} ATTEMPTS REMAINING')
+
+        else:
+            print('It is not this difficult bro...')
+
+        rickGuess = input('A, B, C, D, E or F? ').lower()
+    while lives < 1:
+        print('ROBOT DETECTED, HUMANS ENJOY MUSIC...')
+        break
+
+    print("You have proven yourself so far")
+    print('ACCESS GRANTED')
+    print('Are you sure you want all this power?')
+    response = input('>').lower()
+
+    if response == "yes":
+        print('Are you really unsure?')
+        response = input('>').lower()
+        if response == 'yes':
+            print('Goodybye then')
+
+    else:
+        print('GOODBYE')
+
+    print('Alright then, if youre sure...')
+    print('ENTER PASSWORD...')
+    password = 1752764
+    pw_attempt = input('>').lower()
+    if pw_attempt:
+        if pw_attempt != password:
+            print('FRUADULANT ACTIVITY DETECTED...')
+        else:
+            print('PERMISSIONS GRANTED')
+            pass
+    print('Why havent you given up yet?')
+    print('Most give up by now')
+    print('You were never meant to have access to THE PROGRAM')
+    print('SO JUST STOP TRYING!!')
